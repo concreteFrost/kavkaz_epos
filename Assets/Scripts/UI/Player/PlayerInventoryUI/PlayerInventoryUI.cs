214eecab627d451fb0e5fafb888c7b1b062ed9e4
Ilia Morozov <ilia.m.composer@gmail.com>
2026-03-06 14:29:42 +0000

    getting ready to add consumables

--- Assets/Scripts/UI/Player/PlayerInventoryUI/PlayerInventoryUI.cs
+++ Assets/Scripts/UI/Player/PlayerInventoryUI/PlayerInventoryUI.cs
@@ -1,10 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public enum InventorySection
